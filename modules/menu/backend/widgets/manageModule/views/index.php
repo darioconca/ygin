4c0df6599cbffbf3723a96f26e0576184ed59ec8
cvek <dev@cvek.ru>
2017-08-04 23:44:30 +0300

    reformat module manager widget

--- modules/menu/backend/widgets/manageModule/views/index.php
+++ modules/menu/backend/widgets/manageModule/views/index.php
@@ -8,7 +8,9 @@
 $idInstance = $model->getIdInstance();
 
 $modules = $this->modules;
-if (count($modules) == 0) return;
+if (count($modules) == 0) {
+    return;
+}
 
 $collection = new DaActiveRecordCollection($modules);
 
@@ -18,66 +20,70 @@ $places = $model->modulePlaces;
 // Уже задействованные модули
 $currentIdModules = array();
 $placesArray = array();
-foreach($places AS $place) {
-  $currentIdModules[] = $place->id_module;
-  $placesArray[$place->place][] = $place;
+foreach ($places AS $place) {
+    $currentIdModules[] = $place->id_module;
+    $placesArray[$place->place][] = $place;
 }
 
 // Получили в массиве nid модули, приписанные каким-либо местам
 //+ в $idUpdatedModules идентификаторы этих модулей (пригодится позднее при выводе модулей из архива)
-echo '<div id="modulesSeqPlace">
-      <div class="col-lg-6">
-                <div id="placeNonVisible" class="b-widget-place well">
-                  <h4>Не используемые модули</h4>
-                  <ul>';
+?>
+<div id="modulesSeqPlace">
+    <div class="col-lg-6">
+        <div id="placeNonVisible" class="b-widget-place well affix">
+            <h4>Не используемые модули</h4>
+            <ul>
+                <?php
+                // Если остались модули, которые не видны
+                $position = 0;
+                foreach ($modules AS $m) {
+                    $id = $m->getIdInstance();
+                    if (!in_array($id, $currentIdModules)) {
+                        $position++;
+                        echo "<li id='module_{$id}'>
+                                <input type='hidden' value='' name='mod_{$id}_seq' class='contSeq'>
+                                <input type='hidden' value='' name='mod_{$id}_plc' class='contDid''>
+                                <span class='label label-danger'><sup>{$position}</sup> {$m->name}</span>
+                              </li>";
+                    }
+                } ?>
+            </ul>
+        </div>
+    </div>
+    <div class="col-lg-6">
+    <?php
+    // Пробег по всем положениям модулей
+    $ref = ReferenceElement::model()->byReference(32)->findAll();
+    foreach ($ref as $r) {
+        $idModulePlace = $r->getIdReferenceElement();
+        $placeName = $r->getValue();
 
-// Если остались модули, которые не видны
-$position = 0;
-foreach($modules AS $m) {
-  $id = $m->getIdInstance();
-  if (!in_array($id, $currentIdModules)) {
-    echo '<li id="module_'.$id.'">
-                    <input type="hidden" value="" name="mod_'.$id.'_seq" class="contSeq">
-                    <input type="hidden" value="" name="mod_'.$id.'_plc" class="contDid">
-                    <span class="label label-danger"><sup>'.++$position.'</sup> '.$m->name.'</span>
-                  </li>';
-  }
-}
-echo '    </ul>
-                </div>
-              </div>';
 
-// Пробег по всем положениям модулей
-$ref = ReferenceElement::model()->byReference(32)->findAll();
-
-echo '<div class="col-lg-6">';
-foreach ($ref as $r) {
-  $idModulePlace = $r->getIdReferenceElement();
-  $value = $r->getValue();
-  echo '<div class="b-widget-place well" id="place_'.$idModulePlace.'">
-                  <h4>'.$value.'</h4>
-                  <ul>';
-
-  // После заголовка выводим модули, соответствующие данному местоположению
-  if (array_key_exists($idModulePlace, $placesArray)) {
-    $arrayItem = $placesArray[$idModulePlace];
-    $i = 0;
-    foreach($arrayItem AS $v) {
-      $id = $v->id_module;
-      $i++;
-      $module = $collection->itemAt($id);
-      echo '<li id="module_'.$id.'">
-                      <input type="hidden" value="'.$v->sequence.'" name="mod_'.$id.'_seq" class="contSeq">
-                      <input type="hidden" value="'.$v->place.'" name="mod_'.$id.'_plc" class="contDid">
-                      <span class="label label-success"><sup>'.$i.'</sup> '.$module->name.'</span>
-                    </li>';
-    }
-  }
-  echo '  </ul>
-              </div>';
-}
-echo '</div>
-         </div> <!-- #modSeqPlace -->';
+        // После заголовка выводим модули, соответствующие данному местоположению
+        $placeItems = '';
+        if (array_key_exists($idModulePlace, $placesArray)) {
+            $arrayItem = $placesArray[$idModulePlace];
+            $i = 0;
+            foreach ($arrayItem AS $v) {
+                $id = $v->id_module;
+                $i++;
+                $module = $collection->itemAt($id);
+                $placeItems .= "<li id='module_{$id}'>
+                      <input type='hidden' value='{$v->sequence}' name='mod_{$id}_seq' class='contSeq'>
+                      <input type='hidden' value='{$v->place}' name='mod_{$id}_plc' class='contDid'>
+                      <span class='label label-success'><sup>{$i}</sup> {$module->name}</span>
+                    </li>";
+            }
+        }
+        echo "<div class='b-widget-place well' id='place_{$idModulePlace}' >
+                  <h4>{$placeName} [id={$idModulePlace}]</h4>
+                  <ul>{$placeItems}</ul>
+              </div>";
+    } ?>
+    </div>
+</div>
+<!-- #modSeqPlace -->
+<?php
 Yii::app()->clientScript->registerScript('admin.widgetPlace.init', '
           $(".b-widget-place ul").sortable({
               connectWith: ".b-widget-place ul",
@@ -96,3 +102,5 @@ Yii::app()->clientScript->registerScript('admin.widgetPlace.init', '
               }
           }).disableSelection();
         ', CClientScript::POS_READY);
+
+?>
